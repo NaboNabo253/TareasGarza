56c687a9eed6d76dfe4ef1a628365b17699a3102
Mauricio Garza <gavama2000@gmail.com>
2024-07-27 11:07:57 -0600

    Se crea la funcion de promedio

--- Tarea1/Tarea1.py
+++ Tarea1/Tarea1.py
@@ -13,19 +13,34 @@ def operation_selector(num1, num2, op):
     if not isinstance(op, str):
         return (Error_No_String, None)
 
-    # Verificar si op es uno de los operadores válidos
-    if op not in ('+', '-', '*', '&'):
+    # Verificar si op es uno de los operadores
+    if op not in ['+', '-', '*', '&']:
         return (Error_Operador_Invalido, None)
 
     # Realizar la operación según el valor de op
     if op == '+':
-        result = num1 + num2
+        resultado = num1 + num2
     elif op == '-':
-        result = num1 - num2
+        resultado = num1 - num2
     elif op == '*':
-        result = num1 * num2
+        resultado = num1 * num2
     elif op == '&':
-        result = num1 & num2
+        resultado = num1 & num2
 
     # Devolver el resultado con código de éxito
-    return (SUCCESS, result)
+    return (Exito, resultado)
+
+def calculo_promedio(lista_valores):
+    # Verificar si la lista tiene más de 10 elementos
+    if len(lista_valores) > 10:
+        return (1, None)  # Código de error 1: Lista demasiado grande
+
+    # Verificar si todos los valores en la lista son números
+    for i in lista_valores:
+        if isinstance(i, (int, float)) == False:
+            return (2, None)  # Código de error 2: No todos los valores son números
+
+    # Calcular el promedio si todas las validaciones son exitosas
+    promedio = sum(lista_valores) / len(lista_valores)
+    return (0, promedio)  # Código de éxito 0
+
